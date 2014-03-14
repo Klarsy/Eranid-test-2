85b80f9f1e669ae3be6aa92da0f45afa145c773b
Adrien Levy <adrien.levyklarsy@gmail.com>
2014-03-14 18:31:20 +0100

    fist git shot of eranid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eranid::Application.config.secret_key_base = '6840faa39ed8fc078d6d436f9841e37398fa179b24bcbec3b915a4de44b1ca7f6a76c7284093ec983d8dd5e114ecdd231bed12548521ba164fa471fada1ea896'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eranid::Application.config.session_store :cookie_store, key: '_eranid_session'
